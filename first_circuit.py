82b6ca30b0773944a94ae4aff42d4cfb4751c41a
NeoTRAN001 <neo_tran@outlook.com>
2020-12-26 20:13:22 -0600

    first commit

---
+++ first_circuit.py
@@ -0,0 +1,11 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from numpy import pi
+
+qreg_q = QuantumRegister(1, 'q')
+creg_c = ClassicalRegister(1, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.h(qreg_q[0])
+circuit.t(qreg_q[0])
+circuit.t(qreg_q[0])
+circuit.measure(qreg_q[0], creg_c[0])
